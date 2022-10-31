10ca913e39d22dc9266e9542ad913a531d4e491c
cindyu1020 <cindyu1020@gmail.com>
2022-10-31 10:58:53 -0400

    Add setup files for pods and swiftgen

---
+++ OAidherence/Coordinators/AppCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinator.swift
+//  OAidherence
+//
+//  Created by Yue chen Yu on 2022-10-26.
+//
+
+import UIKit
+
+class AppCoordinator: Coordinator {
+
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = LoginViewController(rootView: LoginView(viewModel: .init(welcome: "Anne")))
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
---
+++ Pods/Target Support Files/Pods-OAidherence/Pods-OAidherence-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OAidherence : NSObject
+@end
+@implementation PodsDummy_Pods_OAidherence
+@end
