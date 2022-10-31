649ede22dffa39d5a87315406b918029d40c1bc3
cindyu1020 <cindyu1020@gmail.com>
2022-10-31 10:49:59 -0400

    Set up initial files for preset colours and fonts

---
+++ OAidherenceCore/Color+Presets.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Presets.swift
+//  OAidherence
+//
+//  Created by Yue chen Yu on 2022-10-30.
+//
+
+import SwiftUI
+
+extension Color {
+    
+//    public static let blackDefault = Color(UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>))
+    
+}
