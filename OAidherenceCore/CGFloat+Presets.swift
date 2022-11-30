f53cf330f0e54132b368cf9af1fedb5eed3ddda3
cindyu1020 <cindyu1020@gmail.com>
2022-11-29 22:50:36 -0500

    Add default padding

---
+++ OAidherenceCore/CGFloat+Presets.swift
@@ -0,0 +1,24 @@
+//
+//  CGFloat+Presets.swift
+//  OAidherence
+//
+//  Created by Yue chen Yu on 2022-11-29.
+//
+
+import Foundation
+
+extension CGFloat {
+    
+    public static let nanoSpace: CGFloat = 4.0
+    
+    public static let miniSpace: CGFloat = 8.0
+    
+    public static let littleSpace: CGFloat = 16.0
+    
+    public static let mediumSpace: CGFloat = 24.0
+    
+    public static let largeSpace: CGFloat = 36.0
+    
+    public static let hugeSpace: CGFloat = 64.0
+    
+}
