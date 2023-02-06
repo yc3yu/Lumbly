7c1711e91f33784bee61f898c6d1e11505292069
Cindy Yu <68297404+yc3yu@users.noreply.github.com>
2023-02-06 10:19:13 -0500

    Add default colours and previews (#10)
    * Add convenience init to UIColor to create colour from hex string
    
    * Add preset colours
    
    * Add preset colour previews
    
    ---------
    
    Co-authored-by: cindyu1020 <cindyu1020@gmail.com>

--- OAidherenceCore/Color+Presets.swift
+++ OAidherenceCore/Color+Presets.swift
@@ -6,9 +6,48 @@
 //
 
 import SwiftUI
+import UIKit
 
 extension Color {
+    public static let prussianBlue   = Color(UIColor(hex: "#00204A") ?? .black)
     
-//    public static let blackDefault = Color(UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>))
+    public static let oysterBay      = Color(UIColor(hex: "#D9FAFF") ?? .black)
     
+    public static let resolutionBlue = Color(UIColor(hex: "#00367D") ?? .black)
+    
+    public static let blueCharcoal   = Color(UIColor(hex: "#000A17") ?? .black)
+    
+    public static let mercuryGray    = Color(UIColor(hex: "#E9E9E9") ?? .black)
+    
+    public static let anakiwaBlue    = Color(UIColor(hex: "#A6F3FF") ?? .black)
+}
+
+struct ColorPresets_Previews: PreviewProvider {
+    static let padding: CGFloat = 5
+    static let boxHeight: CGFloat = 100
+    
+    static var previews: some View {
+        VStack(spacing: ColorPresets_Previews.padding) {
+            HStack(spacing: ColorPresets_Previews.padding) {
+                Rectangle().foregroundColor(Color.prussianBlue)
+                
+                Rectangle().foregroundColor(Color.oysterBay)
+                
+                Rectangle().foregroundColor(Color.resolutionBlue)
+            }
+            .frame(height: ColorPresets_Previews.boxHeight)
+            
+            HStack(spacing: ColorPresets_Previews.padding) {
+                Rectangle().foregroundColor(Color.blueCharcoal)
+                
+                Rectangle().foregroundColor(Color.mercuryGray)
+                
+                Rectangle().foregroundColor(Color.anakiwaBlue)
+            }
+            .frame(height: ColorPresets_Previews.boxHeight)
+            
+            Spacer()
+        }
+        .padding(ColorPresets_Previews.padding)
+    }
 }
