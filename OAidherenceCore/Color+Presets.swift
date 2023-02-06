2686f6093ec0d27446b8053ee12903e1fd4c1e33
cindyu1020 <cindyu1020@gmail.com>
2023-02-06 10:18:11 -0500

    Add preset colours

--- OAidherenceCore/Color+Presets.swift
+++ OAidherenceCore/Color+Presets.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import UIKit
 
 extension Color {
+    public static let prussianBlue   = Color(UIColor(hex: "#00204A") ?? .black)
     
-//    public static let blackDefault = Color(UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>))
+    public static let oysterBay      = Color(UIColor(hex: "#D9FAFF") ?? .black)
     
+    public static let resolutionBlue = Color(UIColor(hex: "#00367D") ?? .black)
+    
+    public static let blueCharcoal   = Color(UIColor(hex: "#000A17") ?? .black)
+    
+    public static let mercuryGray    = Color(UIColor(hex: "#E9E9E9") ?? .black)
+    
+    public static let anakiwaBlue    = Color(UIColor(hex: "#A6F3FF") ?? .black)
 }
