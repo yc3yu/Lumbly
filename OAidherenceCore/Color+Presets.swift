21938085e7a2c89e852d59fcfaa36e32a132d96b
Cindy Yu <68297404+yc3yu@users.noreply.github.com>
2023-02-19 00:52:32 -0500

    Update preset colours (#11)
    * Correct UIColor init from hex
    
    * Update preset colours
    
    ---------
    
    Co-authored-by: cindyu1020 <cindyu1020@gmail.com>

--- OAidherenceCore/Color+Presets.swift
+++ OAidherenceCore/Color+Presets.swift
@@ -11,43 +11,77 @@ import UIKit
 extension Color {
     public static let prussianBlue   = Color(UIColor(hex: "#00204A") ?? .black)
     
-    public static let oysterBay      = Color(UIColor(hex: "#D9FAFF") ?? .black)
+    public static let oysterBay      = Color(UIColor(hex: "#EDFDFF") ?? .black)
     
+    public static let mercuryGrey    = Color(UIColor(hex: "#E9E9E9") ?? .black)
+
+    public static let blueCharcoal   = Color(UIColor(hex: "#000A17") ?? .black)
+
     public static let resolutionBlue = Color(UIColor(hex: "#00367D") ?? .black)
     
-    public static let blueCharcoal   = Color(UIColor(hex: "#000A17") ?? .black)
+    public static let veniceBlue     = Color(UIColor(hex: "#075985") ?? .black)
     
-    public static let mercuryGray    = Color(UIColor(hex: "#E9E9E9") ?? .black)
-    
-    public static let anakiwaBlue    = Color(UIColor(hex: "#A6F3FF") ?? .black)
+    public static let sinbadBlue     = Color(UIColor(hex: "#A8D0D6") ?? .black)
 }
 
 struct ColorPresets_Previews: PreviewProvider {
-    static let padding: CGFloat = 5
-    static let boxHeight: CGFloat = 100
+    static let verticalPadding: CGFloat = 5
+    static let horizontalPadding: CGFloat = 20
+    static let boxWidth: CGFloat = 100
+    static let textWidth: CGFloat = 150
     
     static var previews: some View {
-        VStack(spacing: ColorPresets_Previews.padding) {
-            HStack(spacing: ColorPresets_Previews.padding) {
+        VStack(spacing: ColorPresets_Previews.verticalPadding) {
+            HStack(spacing: ColorPresets_Previews.horizontalPadding) {
                 Rectangle().foregroundColor(Color.prussianBlue)
-                
-                Rectangle().foregroundColor(Color.oysterBay)
-                
-                Rectangle().foregroundColor(Color.resolutionBlue)
+                    .frame(width: ColorPresets_Previews.boxWidth)
+                Text("prussianBlue")
+                    .frame(width: ColorPresets_Previews.textWidth)
             }
-            .frame(height: ColorPresets_Previews.boxHeight)
             
-            HStack(spacing: ColorPresets_Previews.padding) {
-                Rectangle().foregroundColor(Color.blueCharcoal)
-                
-                Rectangle().foregroundColor(Color.mercuryGray)
-                
-                Rectangle().foregroundColor(Color.anakiwaBlue)
+            HStack(spacing: ColorPresets_Previews.horizontalPadding) {
+                Rectangle().foregroundColor(Color.oysterBay)
+                    .frame(width: ColorPresets_Previews.boxWidth)
+                Text("oysterBay")
+                    .frame(width: ColorPresets_Previews.textWidth)
             }
-            .frame(height: ColorPresets_Previews.boxHeight)
+            
+            HStack(spacing: ColorPresets_Previews.horizontalPadding) {
+                Rectangle().foregroundColor(Color.mercuryGrey)
+                    .frame(width: ColorPresets_Previews.boxWidth)
+                Text("mercuryGrey")
+                    .frame(width: ColorPresets_Previews.textWidth)
+            }
+            
+            HStack(spacing: ColorPresets_Previews.horizontalPadding) {
+                Rectangle().foregroundColor(Color.blueCharcoal)
+                    .frame(width: ColorPresets_Previews.boxWidth)
+                Text("blueCharcoal")
+                    .frame(width: ColorPresets_Previews.textWidth)
+            }
+            
+            HStack(spacing: ColorPresets_Previews.horizontalPadding) {
+                Rectangle().foregroundColor(Color.resolutionBlue)
+                    .frame(width: ColorPresets_Previews.boxWidth)
+                Text("resolutionBlue")
+                    .frame(width: ColorPresets_Previews.textWidth)
+            }
+            
+            HStack(spacing: ColorPresets_Previews.horizontalPadding) {
+                Rectangle().foregroundColor(Color.veniceBlue)
+                    .frame(width: ColorPresets_Previews.boxWidth)
+                Text("veniceBlue")
+                    .frame(width: ColorPresets_Previews.textWidth)
+            }
+            
+            HStack(spacing: ColorPresets_Previews.horizontalPadding) {
+                Rectangle().foregroundColor(Color.sinbadBlue)
+                    .frame(width: ColorPresets_Previews.boxWidth)
+                Text("sinbadBlue")
+                    .frame(width: ColorPresets_Previews.textWidth)
+            }
             
             Spacer()
-        }
-        .padding(ColorPresets_Previews.padding)
+        }.padding(ColorPresets_Previews.verticalPadding)
     }
 }
--- OAidherenceCore/UIColor+Hex.swift
+++ OAidherenceCore/UIColor+Hex.swift
@@ -22,9 +22,9 @@ extension UIColor {
             
             if hexColor.count == 6 {
                 if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+                    g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+                    b = CGFloat(hexNumber & 0x0000ff) / 255
                     a = CGFloat(1)
                     
                     self.init(red: r, green: g, blue: b, alpha: a)
