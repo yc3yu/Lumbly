1dea3a52945bae596449e737bc13260664173844
cindyu1020 <cindyu1020@gmail.com>
2023-02-20 21:15:33 -0500

    Update ExerciseTileView previews

--- OAidherence/ExerciseSet/ExerciseTile/ExerciseTileView.swift
+++ OAidherence/ExerciseSet/ExerciseTile/ExerciseTileView.swift
@@ -61,8 +61,12 @@ struct ExerciseTileView_Previews: PreviewProvider {
                 .foregroundColor(.oysterBay)
             
             VStack {
-                ExerciseTileView()
-                    .padding(.mediumSpace)
+                ExerciseTileView(viewModel: .init(exerciseTileData: ExerciseTile(
+                    inlineIcon: "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Physiotherapy.png?sv=2021-10-04&st=2023-02-20T05%3A11%3A41Z&se=2023-02-21T05%3A11%3A41Z&sr=b&sp=r&sig=JU9hfpsfpyvRcz2nBv0QN%2FRv6ItBzqOze61IwY80lls%3D",
+                    exerciseName: "Bird Dog",
+                    reps: "10 Repetitions (per side)",
+                    exerciseImage: "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Rectangle%208.png?sv=2021-10-04&st=2023-02-20T05%3A12%3A06Z&se=2023-02-21T05%3A12%3A06Z&sr=b&sp=r&sig=%2FzlHkdkS8dbdiP46jBFvx3KRasY8mOFuNmGVvuBBmfI%3D")))
+                .padding(.mediumSpace)
                 
                 Spacer()
             }
