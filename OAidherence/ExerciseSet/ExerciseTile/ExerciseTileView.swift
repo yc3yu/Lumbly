3832496b1cccd7a3b6a58df0706dce7fcfe304b2
cindyu1020 <cindyu1020@gmail.com>
2023-02-20 23:55:43 -0500

    Fix AsyncImage resizing

--- OAidherence/ExerciseSet/ExerciseTile/ExerciseTileView.swift
+++ OAidherence/ExerciseSet/ExerciseTile/ExerciseTileView.swift
@@ -29,8 +29,11 @@ struct ExerciseTileView: View {
                 VStack(alignment: .leading) {
                     HStack {
                         if let inlineIcon = viewModel.exerciseTileData?.inlineIcon {
-                            AsyncImage(url: URL(string: inlineIcon))
-                                .frame(width: .mediumSpace, height: .mediumSpace)
+                            AsyncImage(url: URL(string: inlineIcon), content: { image in
+                                image.resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: .mediumSpace, height: .mediumSpace)
+                            }, placeholder: { })
                         }
                         
                         if let exerciseName = viewModel.exerciseTileData?.exerciseName {
@@ -46,8 +49,11 @@ struct ExerciseTileView: View {
                 }
                 
                 if let exerciseImage = viewModel.exerciseTileData?.exerciseImage {
-                    AsyncImage(url: URL(string: exerciseImage))
-                        .frame(width: Constants.exerciseImageWidth, height: Constants.exerciseImageHeight)
+                    AsyncImage(url: URL(string: exerciseImage), content: { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: Constants.exerciseImageWidth, height: Constants.exerciseImageHeight)
+                    }, placeholder: { })
                 }
             }
         }
@@ -62,10 +68,10 @@ struct ExerciseTileView_Previews: PreviewProvider {
             
             VStack {
                 ExerciseTileView(viewModel: .init(exerciseTileData: ExerciseTile(
-                    inlineIcon: "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Physiotherapy.png?sv=2021-10-04&st=2023-02-20T05%3A11%3A41Z&se=2023-02-21T05%3A11%3A41Z&sr=b&sp=r&sig=JU9hfpsfpyvRcz2nBv0QN%2FRv6ItBzqOze61IwY80lls%3D",
+                    inlineIcon: "https://templumblygroup.blob.core.windows.net/lumblyimage/Physiotherapy.png?sv=2021-10-04&st=2023-02-21T04%3A14%3A20Z&se=2023-05-02T03%3A14%3A00Z&sr=b&sp=r&sig=wRkRNDHXQxlZ3QA%2BWFnCGLpMdBvROF0vDp64lO9S57I%3D",
                     exerciseName: "Bird Dog",
                     reps: "10 Repetitions (per side)",
-                    exerciseImage: "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Rectangle%208.png?sv=2021-10-04&st=2023-02-20T05%3A12%3A06Z&se=2023-02-21T05%3A12%3A06Z&sr=b&sp=r&sig=%2FzlHkdkS8dbdiP46jBFvx3KRasY8mOFuNmGVvuBBmfI%3D")))
+                    exerciseImage: "https://templumblygroup.blob.core.windows.net/lumblyimage/Rectangle%208.png?sv=2021-10-04&st=2023-02-21T04%3A15%3A06Z&se=2023-05-02T03%3A15%3A00Z&sr=b&sp=r&sig=5Dtbk8LgQ%2Fyhy6Sq8k8pblmbIKRfPeFnm79%2Ftcq6pas%3D")))
                 .padding(.mediumSpace)
                 
                 Spacer()
