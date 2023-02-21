609748d2a9eb7c04a29e773ee45f66d52da93723
cindyu1020 <cindyu1020@gmail.com>
2023-02-20 23:08:23 -0500

    Create loading view

---
+++ OAidherence/Loading/LoadingView.swift
@@ -0,0 +1,39 @@
+//
+//  LoadingView.swift
+//  OAidherence
+//
+//  Created by Yue chen Yu on 2023-02-20.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    private struct Constants {
+        static let loadingIconSize: CGFloat = 87.0
+    }
+    
+    var loadingText: String?
+    
+    var body: some View {
+        ZStack {
+            Color.oysterBay
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: .largeSpace) {
+                Text(loadingText ?? "")
+                    .font(.title2Bold)
+                    .foregroundColor(.resolutionBlue)
+                
+                Image("Loading")
+                    .resizable()
+                    .frame(width: Constants.loadingIconSize, height: Constants.loadingIconSize)
+            }
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView(loadingText: "Setting up your account...")
+    }
+}
