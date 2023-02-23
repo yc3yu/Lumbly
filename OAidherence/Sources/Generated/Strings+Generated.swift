38208390d27dda5bc667d61d44167c66b623ebb4
cindyu1020 <cindyu1020@gmail.com>
2023-02-22 19:19:01 -0500

    Add strings for ExerciseSetTileView

--- OAidherence/Sources/Generated/Strings+Generated.swift
+++ OAidherence/Sources/Generated/Strings+Generated.swift
@@ -10,6 +10,10 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+  internal enum ExerciseSetTileView {
+    /// Start
+    internal static let start = L10n.tr("Localizable", "ExerciseSetTileView.start", fallback: "Start")
+  }
   internal enum ExerciseSetView {
     /// Start Exercise Set
     internal static let startExerciseSet = L10n.tr("Localizable", "ExerciseSetView.startExerciseSet", fallback: "Start Exercise Set")
