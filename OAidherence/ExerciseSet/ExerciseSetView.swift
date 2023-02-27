43f39fc8ca996e3971d520cc4f5fcb419d352f9e
cindyu1020 <cindyu1020@gmail.com>
2023-02-27 00:55:58 -0500

    Fix button on ExerciseSetView

--- OAidherence/ExerciseSet/ExerciseSetView.swift
+++ OAidherence/ExerciseSet/ExerciseSetView.swift
@@ -13,6 +13,7 @@ struct ExerciseSetView: View {
         static let startButtonWidth: CGFloat = 366.0
         static let startButtonHeight: CGFloat = 50.0
         static let startButtonCornerRadius: CGFloat = 15.0
+        static let bottomPadding: CGFloat = 50.0
     }
     
     @StateObject var viewModel: ExerciseSetViewModel
@@ -22,90 +23,86 @@ struct ExerciseSetView: View {
             Color.oysterBay
                 .edgesIgnoringSafeArea([.leading, .trailing, .bottom])
             
-            VStack(alignment: .leading, spacing: .mediumSpace) {
-                VStack(alignment: .leading, spacing: .nanoSpace) {
-                    if let name = viewModel.exerciseSetData?.name {
-                        Text(name)
-                            .font(.largeTitleBold)
-                    } else {
-                        Text("Lower Back Exercises")
-                            .font(.largeTitleBold)
-                    }
-                    
-                    if let duration = viewModel.exerciseSetData?.duration {
-                        Text(duration)
-                            .font(.title3Regular)
-                    } else {
-                        Text("5 min")
-                            .font(.title3Regular)
-                    }
-                }
-                
-                HStack {
-                    if let physiotherapyIcon = viewModel.exerciseSetData?.physiotherapyIcon {
-                        AsyncImage(url: URL(string: physiotherapyIcon)) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: .mediumSpace, height: .mediumSpace)
-                        } placeholder: {
-                            Color.clear
-                                .frame(width: .mediumSpace, height: .mediumSpace)
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .leading, spacing: .mediumSpace) {
+                    VStack(alignment: .leading, spacing: .nanoSpace) {
+                        if let name = viewModel.exerciseSetData?.name {
+                            Text(name)
+                                .font(.largeTitleBold)
+                        } else {
+                            Text("Lower Back Exercises")
+                                .font(.largeTitleBold)
                         }
-                    } else {
-                        Image("Physiotherapy")
-                            .frame(width: .mediumSpace, height: .mediumSpace)
-                    }
-                    
-                    if let physiotherapyString = viewModel.exerciseSetData?.physiotherapyString {
-                        Text(physiotherapyString)
-                            .foregroundColor(.veniceBlue)
-                    } else {
-                        Text("Chosen by your physiotherapist")
-                            .foregroundColor(.veniceBlue)
-                    }
-                }
-                
-                Divider()
-                
-                if let exerciseTiles = viewModel.exerciseSetData?.exerciseTiles {
-                    ScrollView(showsIndicators: false) {
-                        ForEach(exerciseTiles.indices, id: \.self) { i in
-                            NavigationLink(destination:
-                                            ExerciseInstructionsView(viewModel:
-                                                    .init(exerciseNumber: i + 1,
-                                                          exerciseInstructionsURL: exerciseTiles[i].exerciseInstructionsURL))) {
-                                ExerciseTileView(viewModel:
-                                        .init(exerciseTileData:
-                                                ExerciseTile(inlineIcon: exerciseTiles[i].inlineIcon,
-                                                             exerciseName: exerciseTiles[i].exerciseName,
-                                                             repetitions: exerciseTiles[i].repetitions,
-                                                             exerciseImage: exerciseTiles[i].exerciseImage)))
-                            }.buttonStyle(.plain)
+                        
+                        if let duration = viewModel.exerciseSetData?.duration {
+                            Text(duration)
+                                .font(.title3Regular)
+                        } else {
+                            Text("5 min")
+                                .font(.title3Regular)
                         }
                     }
+                    
+                    HStack {
+                        if let physiotherapyIcon = viewModel.exerciseSetData?.physiotherapyIcon {
+                            AsyncImage(url: URL(string: physiotherapyIcon)) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: .mediumSpace, height: .mediumSpace)
+                            } placeholder: {
+                                Color.clear
+                                    .frame(width: .mediumSpace, height: .mediumSpace)
+                            }
+                        } else {
+                            Image("Physiotherapy")
+                                .frame(width: .mediumSpace, height: .mediumSpace)
+                        }
+                        
+                        if let physiotherapyString = viewModel.exerciseSetData?.physiotherapyString {
+                            Text(physiotherapyString)
+                                .foregroundColor(.veniceBlue)
+                        } else {
+                            Text("Chosen by your physiotherapist")
+                                .foregroundColor(.veniceBlue)
+                        }
+                    }
+                    
+                    Divider()
+                    
+                    if let exerciseTiles = viewModel.exerciseSetData?.exerciseTiles {
+                        ScrollView(showsIndicators: false) {
+                            ForEach(exerciseTiles.indices, id: \.self) { i in
+                                NavigationLink(destination:
+                                                ExerciseInstructionsView(viewModel:
+                                                        .init(exerciseNumber: i + 1,
+                                                              exerciseInstructionsURL: exerciseTiles[i].exerciseInstructionsURL))) {
+                                    ExerciseTileView(viewModel:
+                                            .init(exerciseTileData:
+                                                    ExerciseTile(inlineIcon: exerciseTiles[i].inlineIcon,
+                                                                 exerciseName: exerciseTiles[i].exerciseName,
+                                                                 repetitions: exerciseTiles[i].repetitions,
+                                                                 exerciseImage: exerciseTiles[i].exerciseImage)))
+                                }.buttonStyle(.plain)
+                            }
+                        }
+                    }
+                    
+                    Spacer()
                 }
-                
+                .padding(.top, Constants.topPadding)
+                .padding(.horizontal, .mediumSpace)
+                .padding(.bottom, Constants.bottomPadding)
+            }
+            
+            VStack {
                 Spacer()
                 
-                Button(action: {
-                    
-                }) {
-                    Text(L10n.ExerciseSetView.startExerciseSet)
-                        .font(.title3Bold)
-                        .foregroundColor(.mercuryGrey)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: Constants.startButtonHeight, alignment: .center)
-                        .background(Color.veniceBlue)
-                        .cornerRadius(Constants.startButtonCornerRadius)
-                        .padding(.bottom, .mediumSpace)
-                }
+                BlueButtonView(text: L10n.ExerciseSetView.startExerciseSet, backgroundColor: .veniceBlue, navLinkButton: true)
+                    .frame(width: Constants.startButtonWidth, height: Constants.startButtonHeight)
             }
-            .ignoresSafeArea(.container)
-            .padding(.top, Constants.topPadding)
-            .padding(.horizontal, .mediumSpace)
-            .onAppear(perform: viewModel.fetchData)
         }
+        .onAppear(perform: viewModel.fetchData)
     }
 }
 
