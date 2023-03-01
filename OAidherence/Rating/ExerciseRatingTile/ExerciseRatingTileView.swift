c70ef996f9b5eb79ed7e84316e31fd982cac1006
Cindy Yu <68297404+yc3yu@users.noreply.github.com>
2023-02-28 22:48:59 -0500

    Create ExerciseRatingTIleView (#44)
    * Add images for exercise rating screen
    
    * Add strings for exercise rating view
    
    * Create ExerciseRatingTileView
    
    * Fix spacing on PainLevelTileView
    
    ---------
    
    Co-authored-by: cindyu1020 <cindyu1020@gmail.com>

--- OAidherence/Rating/ExerciseRatingTile/ExerciseRatingTileView.swift
+++ OAidherence/Rating/ExerciseRatingTile/ExerciseRatingTileView.swift
@@ -1,5 +1,5 @@
 //
-//  PainLevelTileView.swift
+//  ExerciseRatingTileView.swift
 //  OAidherence
 //
 //  Created by Yue chen Yu on 2023-02-28.
@@ -7,37 +7,37 @@
 
 import SwiftUI
 
-struct PainLevelTileView: View {
+struct ExerciseRatingTileView: View {
     private struct Constants {
-        static let textHeight: CGFloat = 28.2
-        static let textImageSpacing: CGFloat = 10.24
-        static let tileTopPadding: CGFloat = 14.56
+        static let textHeight: CGFloat = 18.48
+        static let textImageSpacing: CGFloat = 12.0
+        static let tileTopPadding: CGFloat = 11.48
         static let tileSidePadding: CGFloat = 12.0
-        static let tileBottomPadding: CGFloat = 13.0
+        static let tileBottomPadding: CGFloat = 7.66
         static let tileCornerRadius: CGFloat = 10.0
-        static let faceSize: CGFloat = 28.0
+        static let imageSize: CGFloat = 33.38
     }
     
-    @StateObject var viewModel: PainLevelTileViewModel
-
+    @StateObject var viewModel: ExerciseRatingTileViewModel
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: Constants.tileCornerRadius)
                 .foregroundColor(.veniceBlue)
             
-            if let painLevel = viewModel.painLevel {
+            if let exerciseRating = viewModel.exerciseRating {
                 VStack(spacing: Constants.textImageSpacing) {
-                    Text(painLevel)
+                    Text(exerciseRating)
                         .font(.caption1Bold)
                         .multilineTextAlignment(.center)
                         .foregroundColor(.sinbadBlue)
                         .fixedSize(horizontal: false, vertical: true)
                         .frame(height: Constants.textHeight, alignment: .center)
                     
-                    Image(painLevel)
+                    Image(exerciseRating)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: Constants.faceSize, height: Constants.faceSize)
+                        .frame(width: Constants.imageSize, height: Constants.imageSize)
                     
                 }
                 .padding(.top, Constants.tileTopPadding)
@@ -48,18 +48,21 @@ struct PainLevelTileView: View {
     }
 }
 
-struct PainLevelTileView_Previews: PreviewProvider {
+struct ExerciseRatingTileView_Previews: PreviewProvider {
     private struct Constants {
-        static let tileWidth: CGFloat = 87.0
-        static let tileHeight: CGFloat = 94.0
+        static let tileWidth: CGFloat = 88.0
+        static let tileHeight: CGFloat = 83.0
     }
     
     static var previews: some View {
         HStack {
-            PainLevelTileView(viewModel: .init(painLevel: "No pain"))
+            ExerciseRatingTileView(viewModel: .init(exerciseRating: L10n.ExerciseRatingTileView.likedIt))
                 .frame(width: Constants.tileWidth, height: Constants.tileHeight)
             
-            PainLevelTileView(viewModel: .init(painLevel: "Hurts a little more"))
+            ExerciseRatingTileView(viewModel: .init(exerciseRating: L10n.ExerciseRatingTileView.neutral))
+                .frame(width: Constants.tileWidth, height: Constants.tileHeight)
+            
+            ExerciseRatingTileView(viewModel: .init(exerciseRating: L10n.ExerciseRatingTileView.dislikedIt))
                 .frame(width: Constants.tileWidth, height: Constants.tileHeight)
         }
     }
