891ebb1b2dda3c58d82b9a4ef66f360cf3c168e8
cindyu1020 <cindyu1020@gmail.com>
2023-03-16 02:09:02 -0400

    Add recording screen preview

---
+++ OAidherence/Features/RecordingView.swift
@@ -0,0 +1,15 @@
+//
+//  RecordingView.swift
+//  OAidherence
+//
+//  Created by Yue chen Yu on 2023-03-16.
+//
+
+import SwiftUI
+
+struct RecordingView: View {
+    var body: some View {
+        HostedRecordingViewController()
+            .ignoresSafeArea()
+    }
+}
