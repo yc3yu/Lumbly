2743d0f105e26a0552aa184827bb8366f299d5e2
cindyu1020 <cindyu1020@gmail.com>
2023-03-16 22:02:54 -0400

    Move around code in RecordingView so RecordingView struct is at the top

--- OAidherence/Features/Recording/RecordingView.swift
+++ OAidherence/Features/Recording/RecordingView.swift
@@ -7,24 +7,6 @@
 
 import SwiftUI
 
-struct DeviceRotationViewModifier: ViewModifier {
-    let action: (UIDeviceOrientation) -> Void
-
-    func body(content: Content) -> some View {
-        content
-            .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-            }
-    }
-}
-
-extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        self.modifier(DeviceRotationViewModifier(action: action))
-    }
-}
-
 struct RecordingView: View {
     private struct Constants {
         static let recordingButtonSize: CGFloat = 72.0
@@ -94,5 +76,23 @@ struct RecordingView: View {
                 orientation = newOrientation
             }
         }
+    }
+}
+
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
     }
 }
