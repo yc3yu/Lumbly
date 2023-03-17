965b9d20eed28d7fcfa3e0cd46d0f8db87f1e127
cindyu1020 <cindyu1020@gmail.com>
2023-03-16 22:35:16 -0400

    Add ViewControllerLinkable protocol

---
+++ OAidherence/Features/Recording/ViewControllerLinkable.swift
@@ -0,0 +1,10 @@
+//
+//  ViewControllerLinkable.swift
+//  OAidherence
+//
+//  Created by Yue chen Yu on 2023-03-16.
+//
+
+protocol ViewControllerLinkable {
+    func action(_ action : LinkAction)
+}
