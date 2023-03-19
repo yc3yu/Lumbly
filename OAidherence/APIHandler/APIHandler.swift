5bc7f9580d0348082dd9aba177e94bbb9c61b78e
cindyu1020 <cindyu1020@gmail.com>
2023-03-19 18:00:53 -0400

    Add video uploading functionality

--- OAidherence/APIHandler/APIHandler.swift
+++ OAidherence/APIHandler/APIHandler.swift
@@ -8,6 +8,47 @@
 import Foundation
 
 class APIHandler {
+    // MARK: Upload data
+    
+    func uploadVideo(videoFileURL: URL, completion: @escaping () -> ()) {
+        guard let azureConnectionString = ProcessInfo.processInfo.environment["AZURE_STORAGE_CONNECTION_STRING"],
+        let containerName = ProcessInfo.processInfo.environment["AZURE_TEST_USER_CONTAINER_NAME"] else {
+            return
+        }
+        
+        do {
+            let account = try AZSCloudStorageAccount(fromConnectionString: azureConnectionString as String)
+            let blobClient: AZSCloudBlobClient = account.getBlobClient()
+            let blobContainer: AZSCloudBlobContainer = blobClient.containerReference(fromName: containerName)
+
+            blobContainer.createContainerIfNotExists { (error: Error?, exists) -> Void in
+                if error != nil {
+                    print("Error creating container")
+                    print(error.debugDescription)
+                }
+                else {
+                    let fileName = "video/\(videoFileURL.lastPathComponent)"
+                    let blob: AZSCloudBlockBlob = blobContainer.blockBlobReference(fromName: fileName)
+                    
+                    do {
+                        let videoData = try Data(contentsOf: videoFileURL)
+                        let videoInputStream = InputStream(data: videoData)
+                        blob.upload(from: videoInputStream, completionHandler: { _ in
+                            completion()
+                        })
+                    } catch {
+                        print("Error extracting data from URL \(videoFileURL)")
+                    }
+                }
+            }
+        } catch {
+            print("Error uploading video")
+            completion()
+        }
+    }
+    
+    // MARK: Fetch data
+    
     func fetchHomeData(completion: @escaping ((Home) -> ())) {
         if let url = URL(string: APIEndpoints.homeURL) {
             URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
--- OAidherence/Features/Video/Playback/PlaybackViewModel.swift
+++ OAidherence/Features/Video/Playback/PlaybackViewModel.swift
@@ -17,5 +17,22 @@ extension PlaybackView {
             self.apiHandler = APIHandler()
             self.videoFileURL = videoFileURL
         }
+        
+        func uploadVideo() {
+            apiHandler.uploadVideo(videoFileURL: videoFileURL) { [weak self] in
+                if let videoFileURL = self?.videoFileURL {
+                    let path = videoFileURL.path
+                    
+                    guard FileManager.default.fileExists(atPath: path) else { return }
+                    
+                    do {
+                        try FileManager.default.removeItem(atPath: path)
+                        //for testing
+                    } catch {
+                        print("Error removing file at url: \(videoFileURL)")
+                    }
+                }
+            }
+        }
     }
 }
