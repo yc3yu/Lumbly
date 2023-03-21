48414990359cf67c1a9437d5fc35543b6b31a9a7
cindyu1020 <cindyu1020@gmail.com>
2023-03-21 02:22:13 -0400

    Fix scrolling on SignupView

--- OAidherence/Features/Onboarding/Signup/SignupView.swift
+++ OAidherence/Features/Onboarding/Signup/SignupView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SignupView: View {
     private struct Constants {
+        static let topPadding: CGFloat = 40.0
         static let logoWidth: CGFloat = 282.0
         static let logoHeight: CGFloat = 103.0
         static let fieldHeight: CGFloat = 56.0
@@ -28,45 +29,49 @@ struct SignupView: View {
             Color.oysterBay
                 .ignoresSafeArea(.all)
             
-            VStack(spacing: Constants.vStackSpacing) {
-                Image("LumblyMedium")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: Constants.logoWidth, height: Constants.logoHeight)
-                
-                StyledTextFieldView(textFieldContent:
-                                        AnyView(TextField(L10n.Onboarding.physiotherapistCode,
-                                                          text: $physiotherapistCode)
-                                            .textInputAutocapitalization(.never)
-                                            .autocorrectionDisabled(true)))
-                
-                StyledTextFieldView(textFieldContent:
-                                        AnyView(TextField(L10n.Onboarding.name,
-                                                          text: $name)
-                                            .textInputAutocapitalization(.never)
-                                            .autocorrectionDisabled(true)))
-                
-                StyledTextFieldView(textFieldContent:
-                                        AnyView(TextField(L10n.Onboarding.email,
-                                                          text: $email)
-                                            .textInputAutocapitalization(.never)
-                                            .autocorrectionDisabled(true)))
-                
-                StyledTextFieldView(textFieldContent:
-                                        AnyView(SecureField(L10n.Onboarding.password,
-                                                            text: $password)
-                                            .textInputAutocapitalization(.never)
-                                            .autocorrectionDisabled(true)))
-                
-                NavigationLink(destination: HomeView()) {
-                    BlueButtonView(text: L10n.Onboarding.signUp,
-                                   navLinkButton: true)
-                    .frame(width: Constants.buttonWidth, height: Constants.buttonHeight)
-                }.disabled(physiotherapistCode.isEmpty ||
-                           name.isEmpty ||
-                           email.isEmpty ||
-                           password.isEmpty)
-            }.padding(.horizontal, Constants.vStackHorizontalPadding)
+            ScrollView {
+                VStack(spacing: Constants.vStackSpacing) {
+                    Image("LumblyMedium")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: Constants.logoWidth, height: Constants.logoHeight)
+                        .padding(.top, Constants.topPadding)
+                    
+                    StyledTextFieldView(textFieldContent:
+                                            AnyView(TextField(L10n.Onboarding.physiotherapistCode,
+                                                              text: $physiotherapistCode)
+                                                .textInputAutocapitalization(.never)
+                                                .autocorrectionDisabled(true)))
+                    
+                    StyledTextFieldView(textFieldContent:
+                                            AnyView(TextField(L10n.Onboarding.name,
+                                                              text: $name)
+                                                .textInputAutocapitalization(.never)
+                                                .autocorrectionDisabled(true)))
+                    
+                    StyledTextFieldView(textFieldContent:
+                                            AnyView(TextField(L10n.Onboarding.email,
+                                                              text: $email)
+                                                .textInputAutocapitalization(.never)
+                                                .autocorrectionDisabled(true)))
+                    
+                    StyledTextFieldView(textFieldContent:
+                                            AnyView(SecureField(L10n.Onboarding.password,
+                                                                text: $password)
+                                                .textInputAutocapitalization(.never)
+                                                .autocorrectionDisabled(true)))
+                    
+                    NavigationLink(destination: HomeView()) {
+                        BlueButtonView(text: L10n.Onboarding.signUp,
+                                       navLinkButton: true)
+                        .frame(width: Constants.buttonWidth, height: Constants.buttonHeight)
+                        .padding(.bottom, Constants.vStackSpacing)
+                    }.disabled(physiotherapistCode.isEmpty ||
+                               name.isEmpty ||
+                               email.isEmpty ||
+                               password.isEmpty)
+                }.padding(.horizontal, Constants.vStackHorizontalPadding)
+            }
         }
     }
 }
