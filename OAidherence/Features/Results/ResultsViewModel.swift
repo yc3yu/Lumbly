545c05dab7e7941b1fd7438da545109f96b511f1
cindyu1020 <cindyu1020@gmail.com>
2023-04-01 00:59:49 -0400

    Add loading and error states in ResultsView

--- OAidherence/Features/Results/ResultsViewModel.swift
+++ OAidherence/Features/Results/ResultsViewModel.swift
@@ -29,6 +29,10 @@ struct IndividualExerciseResults: Decodable {
 
 extension ResultsView {
     class ResultsViewModel: ObservableObject {
+        private struct Constants {
+            static let requestDelay: Double = 1.0
+        }
+        
         private let apiHandler: APIHandler
         private var recordingViewModel: RecordingView.RecordingViewModel
         
@@ -43,8 +47,40 @@ extension ResultsView {
         }
 
         func fetchData() {
-            apiHandler.fetchResultsData() { [weak self] results in
+            isLoading = true
+            
+            guard let timestamp = recordingViewModel.timestamp else {
+                dataAvailability.status = .error
+                isLoading = false
+                return
+            }
+            
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ [weak self] currentTimer in
+                if let parentExerciseSet = self?.recordingViewModel.parentExerciseSet,
+                   let exerciseName = self?.recordingViewModel.exerciseName {
+                    self?.apiHandler.fetchResultsDataAvailability(
+                        parentExerciseSet: parentExerciseSet,
+                        exerciseName: exerciseName,
+                        timestamp: timestamp) { [weak self] dataAvailability in
+                            self?.dataAvailability = dataAvailability
+                        }
+                }
+                
+                if self?.dataAvailability.status != .unavailable {
+                    currentTimer.invalidate()
+                }
+            }
+            
+            guard dataAvailability.status == .available else {
+                isLoading = false // TODO: Check if this is necessary (depends on implementation)
+                return
+            }
+            
+            apiHandler.fetchResultsData(parentExerciseSet: recordingViewModel.parentExerciseSet,
+                                        exerciseName: recordingViewModel.exerciseName,
+                                        timestamp: timestamp) { [weak self] results in
                 self?.results = results
+                self?.isLoading = false
             }
         }
     }
