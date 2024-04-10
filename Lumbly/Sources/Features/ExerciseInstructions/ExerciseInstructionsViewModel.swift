4c24b3a50d161f6a0551b6ee2b3c595d0aef108b
cindyu1020 <cindyu1020@gmail.com>
2024-04-09 19:22:58 -0700

    Remove @Published from exerciseNumber

--- Lumbly/Sources/Features/ExerciseInstructions/ExerciseInstructionsViewModel.swift
+++ Lumbly/Sources/Features/ExerciseInstructions/ExerciseInstructionsViewModel.swift
@@ -20,11 +20,11 @@ extension ExerciseInstructionsView {
     class ExerciseInstructionsViewModel: ObservableObject {
         private let apiHandler: APIHandler
 
-        @Published private(set) var exerciseNumber: Int
         @Published private(set) var exerciseInstructionsData: ExerciseInstructions? = nil
         @Published private(set) var isLoading: Bool = false
         @Published var showReadyButton: Bool
         
+        private(set) var exerciseNumber: Int
         private(set) var exerciseInstructionsURL: String?
         private(set) var parentExerciseSet: String
 
