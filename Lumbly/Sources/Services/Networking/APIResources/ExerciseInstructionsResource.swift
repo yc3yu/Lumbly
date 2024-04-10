16bd9a6ba7ae4e7580898adeb8c3ab0350db3aba
cindyu1020 <cindyu1020@gmail.com>
2024-04-10 02:03:41 -0700

    Add ExerciseInstructionsResource

---
+++ Lumbly/Sources/Services/Networking/APIResources/ExerciseInstructionsResource.swift
@@ -0,0 +1,12 @@
+//
+//  ExerciseInstructionsResource.swift
+//  Lumbly
+//
+//  Created by Yue chen Yu on 2024-04-09.
+//
+
+struct ExerciseInstructionsResource: APIResource {
+    typealias ModelType = ExerciseInstructions
+    
+    var url: URL?
+}
