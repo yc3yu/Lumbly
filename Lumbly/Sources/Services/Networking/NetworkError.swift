56a7d03278bbed0dd81b5f23118cdf8aa04eed71
cindyu1020 <cindyu1020@gmail.com>
2024-04-22 12:50:48 -0700

    Move HTTP-related errors to nested enum

--- Lumbly/Sources/Services/Networking/NetworkError.swift
+++ Lumbly/Sources/Services/Networking/NetworkError.swift
@@ -9,15 +9,16 @@ import Foundation
 
 enum NetworkError: Error {
     case malformedURL
-
-    case badRequest
-    case unauthorized
-    case paymentRequired
-    case forbidden
-    case notFound
-    case requestEntityTooLarge
-    case unprocessableEntity
-    case unknown(data: Data, response: URLResponse)
-
     case invalidResponse(data: Data)
+    
+    enum HTTPError: Error {
+        case badRequest
+        case unauthorized
+        case paymentRequired
+        case forbidden
+        case notFound
+        case requestEntityTooLarge
+        case unprocessableEntity
+        case unknown(data: Data, response: URLResponse)
+    }
 }
