6f08619306a69203de39c0d129ba65fd934fc4bc
Cindy Yu <68297404+yc3yu@users.noreply.github.com>
2024-04-27 00:15:35 -0700

    Set placeholder physiotherapy icon from assets (#112)
    Co-authored-by: cindyu1020 <cindyu1020@gmail.com>

--- Lumbly/Resources/SwiftGen/Generated/Strings+Generated.swift
+++ Lumbly/Resources/SwiftGen/Generated/Strings+Generated.swift
@@ -55,6 +55,8 @@ internal enum L10n {
   internal enum ExerciseSetView {
     /// There are no exercises to show for this exercise set.
     internal static let noExercises = L10n.tr("Localizable", "ExerciseSetView.noExercises", fallback: "There are no exercises to show for this exercise set.")
+    /// Physiotherapy
+    internal static let physiotherapyIcon = L10n.tr("Localizable", "ExerciseSetView.physiotherapyIcon", fallback: "Physiotherapy")
     /// Start Exercise Set
     internal static let startExerciseSet = L10n.tr("Localizable", "ExerciseSetView.startExerciseSet", fallback: "Start Exercise Set")
   }
