93b024c0b0ca6be2566aaf45675f9e93db7745fd
cindyu1020 <cindyu1020@gmail.com>
2024-05-13 16:37:54 -0700

    Add localized string "Please fill in all required fields to continue."

--- Lumbly/Resources/Generated/Strings.swift
+++ Lumbly/Resources/Generated/Strings.swift
@@ -120,6 +120,8 @@ internal enum L10n {
   internal enum Onboarding {
     /// Email
     internal static let email = L10n.tr("Localizable", "Onboarding.email", fallback: "Email")
+    /// Please fill in all required fields to continue.
+    internal static let fillInAllRequiredFields = L10n.tr("Localizable", "Onboarding.fillInAllRequiredFields", fallback: "Please fill in all required fields to continue.")
     /// Log in
     internal static let logIn = L10n.tr("Localizable", "Onboarding.logIn", fallback: "Log in")
     /// Name
