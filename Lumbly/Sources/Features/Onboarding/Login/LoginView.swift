5d9bf3ea619a97692e158503bbf575a713562be5
cindyu1020 <cindyu1020@gmail.com>
2024-05-17 18:40:30 -0700

    Update LoginView to use LumblyInputView

--- Lumbly/Sources/Features/Onboarding/Login/LoginView.swift
+++ Lumbly/Sources/Features/Onboarding/Login/LoginView.swift
@@ -8,48 +8,22 @@
 import SwiftUI
 
 struct LoginView: View {
-    private struct Constants {
-        static let logoWidth: CGFloat = 282.0
-        static let logoHeight: CGFloat = 103.0
-        static let fieldHeight: CGFloat = 56.0
-        static let buttonWidth: CGFloat = 177.0
-        static let buttonHeight: CGFloat = 56.0
-        static let vStackSpacing: CGFloat = 40.0
-        static let vStackHorizontalPadding: CGFloat = 20.0
-    }
-    
     @State private var email: String = ""
     @State private var password: String = ""
     
     var body: some View {
-        ZStack {
-            Color.oysterBay
-                .ignoresSafeArea(.all)
-            
-            VStack(spacing: Constants.vStackSpacing) {
-                Image(asset: Asset.lumblyMedium)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: Constants.logoWidth, height: Constants.logoHeight)
-                
-                StyledTextFieldView(viewModel:
-                        .init(L10n.Onboarding.email,
-                              text: $email,
-                              autocorrectionDisabled: true))
-                
-                StyledTextFieldView(viewModel:
-                        .init(L10n.Onboarding.password,
-                              text: $password,
-                              autocorrectionDisabled: true,
-                              isSecureField: true))
-                
-                NavigationLink(destination: HomeView()) {
-                    BlueButtonView(text: L10n.Onboarding.logIn,
-                                   navLinkButton: true)
-                    .frame(width: Constants.buttonWidth, height: Constants.buttonHeight)
-                }.disabled(email.isEmpty || password.isEmpty)
-            }.padding(.horizontal, Constants.vStackHorizontalPadding)
-        }
+        LumblyInputView(viewModel: .init(
+            styledTextFieldViewModels: [
+                .init(L10n.Onboarding.email,
+                      text: $email,
+                      autocorrectionDisabled: true),
+                .init(L10n.Onboarding.password,
+                      text: $password,
+                      autocorrectionDisabled: true,
+                      isSecureField: true)
+            ],
+            buttonText: L10n.Onboarding.logIn,
+            buttonDestination: HomeView()))
     }
 }
 
