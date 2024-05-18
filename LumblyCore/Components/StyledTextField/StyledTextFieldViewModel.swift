d77a4cd18607842712f0af97b404e2ab88e8f347
cindyu1020 <cindyu1020@gmail.com>
2024-05-17 17:01:49 -0700

    Move styledTitle from view to view model

--- LumblyCore/Components/StyledTextField/StyledTextFieldViewModel.swift
+++ LumblyCore/Components/StyledTextField/StyledTextFieldViewModel.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 extension StyledTextFieldView {
     class ViewModel: ObservableObject {
-        @Binding var text: String
-        
         private(set) var title: String
+        private(set) var styledTitle: String
         private(set) var isRequiredField: Bool
         private(set) var autocapitalization: TextInputAutocapitalization
         private(set) var autocorrectionDisabled: Bool
         private(set) var backgroundColor: Color
         private(set) var axis: Axis
         private(set) var isSecureField: Bool
+
+        @Binding var text: String
         
         init(_ title: String = "",
              text: Binding<String>,
@@ -35,6 +36,9 @@ extension StyledTextFieldView {
             self.backgroundColor = backgroundColor
             self.axis = axis
             self.isSecureField = isSecureField
+            
+            let suffix = isRequiredField ? "*" : ""
+            self.styledTitle = title + suffix
         }
     }
 }
